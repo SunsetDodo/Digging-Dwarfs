a77ea01535039b7b280b3ab7277ac83bfa949f89
Nathaniel Malichi <nathanielmalichi@gmail.com>
2025-04-06 09:23:52 +0300

    Basic Camera Movement
    Will improve if time allows

--- Digging Dwarfs/Assets/Scripts/CameraMovement.cs
+++ Digging Dwarfs/Assets/Scripts/CameraMovement.cs
@@ -6,18 +6,27 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Transform player;   
     [SerializeField] private Rigidbody2D playerRB;
+
+    [SerializeField] private float zoomSmoothness;
+    [SerializeField] private float minFOV;
+    [SerializeField] private float maxFOV;
+
+    [SerializeField] private float movementSmoothness;
     
-    
+    private Camera _camera;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var targetFOV = Mathf.Lerp(minFOV, maxFOV, playerRB.velocity.magnitude *  playerRB.velocity.magnitude);
+        _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, targetFOV, zoomSmoothness * Time.deltaTime);
         
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, new Vector3(player.position.x, player.position.y, -10), movementSmoothness * Time.deltaTime);
     }
 }
